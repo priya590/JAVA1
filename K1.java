39da1c65f269d80904632911d6254bc04b43e904
priya590 <priya.23cs105@sode-edu.in>
2024-09-18 19:52:28 +0530

    Add files via upload

---
+++ K1.java
@@ -0,0 +1,12 @@
+public class K1{
+    public static void main(String[] args){
+        int n1=8;
+	int n2=4;
+	int sum1=n1+n2;
+	int dif1=n1-n2;
+	int prd1=n1*n2;
+	int div1=n1/n2;
+	int rem1=n1%n2;
+	System.out.println(sum1);
+    }
+}
