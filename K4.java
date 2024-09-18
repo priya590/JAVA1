3ecd7d8047c38677b19018dc3fb615cecb6ecc78
priya590 <priya.23cs105@sode-edu.in>
2024-09-18 19:55:46 +0530

    Add files via upload

---
+++ K4.java
@@ -0,0 +1,13 @@
+public class K4{
+    public static void main(String[] args){
+	char ch1, ch2;
+	ch1 = 88;
+	ch2 = 'Y';
+	System.out.println(ch1 + " " + ch2);
+
+	int num1,num2;
+	num1 = 88;
+	System.out.println(num1 + " " + ch2);
+
+    }
+}
